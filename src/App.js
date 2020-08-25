b126337c466cf24df7cce5346fd086547e22d738
Rashmi Gupta <rashmi224141@gmail.com>
2020-08-25 09:33:52 +0530

    added option to choose pokemon card count

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import PokeGallery from './pokeGallery';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {count: 5, isSubmit: false};
+    this.updateCount = this.updateCount.bind(this);
+    this.toggleSubmit = this.toggleSubmit.bind(this);
+  }
+
+  updateCount({target}) {
+    this.setState(() => ({count: target.value}));
+  }
+
+  toggleSubmit() {
+    this.setState((state) => ({isSubmit: !state.isSubmit}));
+  }
+
+  render() {
+    let countBox = (
+      <input placeholder="Name" onKeyUp={this.updateCount}></input>
+    );
+    let buttonText = 'Show pokemons';
+
+    if (this.state.isSubmit) {
+      buttonText = 'Change count';
+      countBox = (
+        <PokeGallery count={+this.state.count}>Pokemon Gallery</PokeGallery>
+      );
+    }
+
+    return (
+      <div class="App">
+        {countBox}
+        <button onClick={this.toggleSubmit}>{buttonText}</button>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/pokeGallery.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PokeGallery = function (props) {
+  return <div>{props.count}</div>;
+};
+
+export default PokeGallery;
